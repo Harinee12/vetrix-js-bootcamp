de2b0a2ee1146788a63d7513b78ffb16c126f0d5
Harini <harinigovindarajan05@gmail.com>
2026-06-18 18:16:01 +0530

    Added Day 2 JavaScript Bootcamp Task

---
+++ app1.js
@@ -0,0 +1,7 @@
+let name = "Harini";
+let age = 20;
+let codedBefore = true;
+
+console.log(
+  `Hi! I'm ${name}, a ${age}-year-old student. I have coded before: ${codedBefore}, and I'm excited to start my JavaScript learning journey.`,
+);
