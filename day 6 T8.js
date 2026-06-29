f2d9f013ffafe0bd21ff4f978455d253201c50c4
aathiraradhakrishnan1999-ctrl <aathiraradhakrishnan1999@gmail.com>
2026-06-29 22:43:02 +0530

    feat: merge task and feature data streams

---
+++ day 6 T8.js
@@ -0,0 +1,16 @@
+let internTasks = [
+  "Setup Environment",
+  "Completed JavaScript Basics",
+  "Write Functions",
+  "Practice Arrays",
+  "Submit Assignment"
+];
+
+let vetrixAppFeatures = [
+  "Login Screen",
+  "Dashboard UI"
+];
+
+let workspaceRegistry = internTasks.concat(vetrixAppFeatures);
+
+console.log(workspaceRegistry);
