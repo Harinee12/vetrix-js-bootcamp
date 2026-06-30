1280c8bac5cf720f104bbd9bfdd8bf77900a1dbf
Harini G <23it1296@stjosephstechnology.ac.in>
2026-06-30 20:46:39 +0530

    day 5,6,7,8,9,10

---
+++ day6.js
@@ -0,0 +1,13 @@
+let internTasks = [
+  "CompleteAssignment",
+  "SetupEnvironment",
+  "LearnVariables",
+  "WriteFunctions",
+  "LearnArrays",
+];
+console.log("Before removing tasks:");
+console.log(internTasks);
+internTasks.shift();
+internTasks.pop();
+console.log("Remaining tasks:");
+console.log(internTasks);
