701b92bc07e931ab55a0dbc0cd45af35b0d17dc5
aathiraradhakrishnan1999-ctrl <aathiraradhakrishnan1999@gmail.com>
2026-07-02 20:46:04 +0530

    feat: architect initial object appState

---
+++ day9 T15.js
@@ -0,0 +1,16 @@
+// Create a global object
+let appState = {
+    workspaceName: "Vetrix JS Bootcamp",
+    activeInterns: 5,
+    isProduction: false,
+    allowedModules: [
+        "Setup Environment",
+        "Learn Variables",
+        "Write Functions",
+        "Login Screen",
+        "Dashboard UI"
+    ]
+};
+
+// Display the object
+console.log(appState);
