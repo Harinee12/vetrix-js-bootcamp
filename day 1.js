a223dec4cc0c01a3d1326d3cc7323af82c607eaf
aathiraradhakrishnan1999-ctrl <aathiraradhakrishnan1999@gmail.com>
2026-07-10 13:48:08 +0530

    Add Day 1 and Day 2 tasks

---
+++ day 1.js
@@ -0,0 +1,5 @@
+let name = "Aathira";
+let age = 22;
+let hasCodedBefore = true;
+
+console.log("Hi, my name is " + name + ". I am " + age + " years old, and I have coded before: " + hasCodedBefore + ".");
--- day 2.js
+++ day 2.js
@@ -1,16 +1,13 @@
 let birthYear = Number(prompt("Enter your birth year:"));
 
-if (birthYear > 2026 || birthYear < 1900) {
-    alert("Invalid birth year entered!");
+if (birthYear < 1900 || birthYear > 2026) {
+    alert("Invalid birth year!");
 } else {
     let age = 2026 - birthYear;
 
     if (age >= 18) {
         alert("You are eligible to vote!");
-        console.log("You are eligible to vote!");
     } else {
         alert("You are not old enough to vote yet.");
-        console.log("You are not old enough to vote yet.");
     }
-} 
-console.log("Testing push");
+}
